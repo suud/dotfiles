726c7169d8f193f28896f9850637e9ddae6597b2
Timo Sutterer <hi@timo-sutterer.de>
2020-06-06 15:26:19 +0200

    Add alias to start temporary dev container

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
-# automatically ls after every successfull cd:
+alias dev='docker run -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)":/app -it --rm suud/dotfiles'
+# automatically ls after every cd
 function cd {
     builtin cd "$@" && ls
 }
